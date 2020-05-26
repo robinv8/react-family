7d1f8aaf498d5757991efec3e38b1b014c898daf
robin <robin@rnode.me>
2020-05-26 16:29:26 +0800

    feat: 添加新功能

--- src/test.js
+++ src/test.js
@@ -3,6 +3,8 @@
  * @Date: 2020-05-26 15:18:26
  * @Author:
  * @LastEditors: robin
- * @LastEditTime: 2020-05-26 16:09:43
+ * @LastEditTime: 2020-05-26 16:29:06
  */
+console.log(1);
+console.log(1);
 console.log(1);
