daf9dd0e689a161aad79a03fe5d10c920e8f986d
robin <robin@rnode.me>
2020-05-26 16:54:54 +0800

    fix: :bug: 修复bug

--- src/test2.js
+++ src/test2.js
@@ -3,6 +3,7 @@
  * @Date: 2020-05-26 16:32:30
  * @Author:
  * @LastEditors: robin
- * @LastEditTime: 2020-05-26 16:32:38
+ * @LastEditTime: 2020-05-26 16:54:20
  */
+console.log(1);
 console.log(1);
