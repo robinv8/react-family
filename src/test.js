3cdc42dbb2eb6bf3bb54b9931aade7f2be0f5eb6
robin <robin@rnode.me>
2020-05-26 16:31:06 +0800

    revert: 版本回滚

--- src/test.js
+++ src/test.js
@@ -3,8 +3,9 @@
  * @Date: 2020-05-26 15:18:26
  * @Author:
  * @LastEditors: robin
- * @LastEditTime: 2020-05-26 16:30:10
+ * @LastEditTime: 2020-05-26 16:30:34
  */
+console.log(1);
 console.log(1);
 console.log(1);
 console.log(1);
