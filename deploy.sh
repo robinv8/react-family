8330cf083fc4999d32a59c9573d1d4ee1d6ded74
robin <robin@rnode.me>
2020-06-18 18:13:27 +0800

    chore: test

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
  # @Date: 2020-06-18 13:54:15
  # @Author: robin
  # @LastEditors: robin
- # @LastEditTime: 2020-06-18 18:12:17
+ # @LastEditTime: 2020-06-18 18:13:20
 ### 
 #!/bin/bash
 echo "build start"
@@ -22,7 +22,7 @@ echo "-------------------------------------------------------------------------"
 
 git add .
 git commit -m "build: 前端构建"
-git pull origin $(git rev-parse --abbrev-ref HEAD) --allow-unrelated-histories 
+git pull --rebase origin $(git rev-parse --abbrev-ref HEAD) --allow-unrelated-histories 
 git push origin $(git rev-parse --abbrev-ref HEAD) --no-verify
 
 echo "-------------------------------------------------------------------------"
