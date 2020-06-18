3cb0b49a030cc88119020364d17977ced61680c1
robin <robin@rnode.me>
2020-06-18 18:15:36 +0800

    chore: test

--- deploy.sh
+++ deploy.sh
@@ -3,16 +3,16 @@
  # @Date: 2020-06-18 13:54:15
  # @Author: robin
  # @LastEditors: robin
- # @LastEditTime: 2020-06-18 18:13:20
+ # @LastEditTime: 2020-06-18 18:15:28
 ### 
 #!/bin/bash
 echo "build start"
 echo "-------------------------------------------------------------------------"
 
 
-git pull origin $(git rev-parse --abbrev-ref HEAD)
-git merge origin/master
-npm run build
+# git pull origin $(git rev-parse --abbrev-ref HEAD)
+# git merge origin/master
+# npm run build
 
 echo "-------------------------------------------------------------------------"
 echo "build end"
